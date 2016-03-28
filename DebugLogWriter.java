653d2383f6bb4fc863672811a15704cbdd47d717
iamfuzi <iamfuzi@gmail.com>
2016-03-28 11:20:45 +0800

    log util for GUI

---
+++ DebugLogWriter.java
@@ -0,0 +1,44 @@
+package com.hie.cloverleaf.util;
+
+import java.io.File;
+import java.io.IOException;
+
+import com.hie.cloverleaf.install.ClientInstallPaths;
+import com.hie.util.LogWriter;
+
+public class DebugLogWriter {
+    
+    public static final String LOG_FILE_PATH = ClientInstallPaths.getInstallBaseDir() + File.separator + "debugLog.log";
+    
+    public static final String LOG_NAME = "debugLog";
+    
+    static LogWriter writer = null;
+    
+    static DebugLogWriter instance = null;
+    
+    public static DebugLogWriter getInstance()
+    {
+        if(instance != null)
+            return instance;
+        else
+            return new DebugLogWriter();       
+    }
+    
+    
+    public static void log(String msg)
+    {
+        try {
+            getInstance().getLogWriter().logWarningMessage(msg);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public LogWriter getLogWriter() throws IOException
+    {
+        if(null == writer)
+            writer = LogWriter.log(LOG_FILE_PATH, LOG_NAME);
+        return writer;
+    }
+    
+}
